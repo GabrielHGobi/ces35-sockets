953c6f14c964ebcae3d39ef188a15f46e2971200
GabrielHGobi <gabrielhgobi@gmail.com>
2022-09-12 15:45:23 -0300

    Error handling on making connection cli-server

--- web-client.py
+++ web-client.py
@@ -2,8 +2,53 @@ import argparse
 
 server_parser = argparse.ArgumentParser(description='List URL\'s to files to be requested by the client')
 
-server_parser.add_argument('URL', metavar='URL', type=str,  nargs='+',
+def client():
+    server_parser = ArgumentParser(description='List URL\'s to files to be requested by the client')
+    server_parser.add_argument('URL_list', metavar='URL', type=str,  nargs='+',
                             help='file URL to be retrieved')
+    args = server_parser.parse_args()
 
-args = server_parser.parse_args()
-print(args.URL)
+    for URL in args.URL_list:
+
+        # server machine's name 
+        serverHostname = url_parsed['hostname']
+
+        # server port number
+        serverPort = url_parsed['port']
+
+        # create TCP socket on client to use for connecting to remote
+        # server.  Indicate the server's remote listening port
+        try: 
+            clientSocket = socket(AF_INET, SOCK_STREAM) 
+        except error as e: 
+            print ("Error creating socket: %s" % e) 
+            sys.exit(1)
+
+        # retrive the IP of the server
+        try:
+            IP_server = gethostbyname(serverHostname)
+        except gaierror:
+            print("Invalid hostname in URL: %s: %s" % (URL, serverHostname))
+            sys.exit(1)
+
+        # open the TCP connection
+        try: 
+            clientSocket.connect((IP_server, serverPort))
+        except gaierror as e: 
+            print("Address-related error connecting to server: %s" % e) 
+            sys.exit(1) 
+        except error as e: 
+            print("Connection error: %s" % e) 
+            sys.exit(1)
+
+        # construct and send HTTP request
+
+        # close the TCP connection
+        try: 
+            clientSocket.close()
+        except error as e: 
+            print ("Error closing socket: %s" % e) 
+            sys.exit(1)
+
+if __name__ == '__main__':
+    client()
--- web-server.py
+++ web-server.py
@@ -1,14 +1,74 @@
-import argparse
+from argparse import ArgumentParser
+from threading import Thread
+from socket import *
+import sys
+import os
 
-server_parser = argparse.ArgumentParser(description='List the address of the web server \
-    with host and port and the directory where the files will be served')
+class ClientThread(Thread):
+    def __init__(self, clientAddr, clientConn):
+        Thread.__init__(self)
+        self.cConn = clientConn
+        print("New connection added: ", clientAddr)
 
-server_parser.add_argument('Host', metavar='host',type=str,
-                            help='the hostname of the web server')
-server_parser.add_argument('Port', metavar='port', type=str,
-                            help='the port of the webs erver')
-server_parser.add_argument('Dir', metavar='dir', type=str,
-                            help='the directory where the files will be served')
+    def run(self):
+        pass
 
-args = server_parser.parse_args()
-print(args.Host, args.Port, args.Dir)
+def server():
+    server_parser = ArgumentParser(description='List the address of the web server \
+        with host and port and the directory where the files will be served')
+    server_parser.add_argument('Host', metavar='host',type=str,
+                                help='the hostname of the web server')
+    server_parser.add_argument('Port', metavar='port', type=int,
+                                help='the port of the webs erver')
+    server_parser.add_argument('Dir', metavar='dir', type=str,
+                                help='the directory where the files will be served')
+    args = server_parser.parse_args()
+
+    # create TCP welcoming socket
+    try: 
+        serverSocket = socket(AF_INET, SOCK_STREAM) 
+    except error as e: 
+        print ("Error creating socket: %s" % e) 
+        sys.exit(1)
+
+    # retrive the IP of the host
+    try:
+        IP_host = gethostbyname(args.Host)
+    except gaierror:
+        print("Invalid hostname: %s" % args.Host)
+        sys.exit(1)
+
+    # open a TCP connection
+    try: 
+        serverSocket.bind((IP_host, args.Port)) 
+    except gaierror as e: 
+        print("Address-related error binding to server: %s" % e) 
+        sys.exit(1) 
+    except error as e: 
+        print("Binding error: %s" % e) 
+        sys.exit(1)
+
+    # check if the dir passed is valid
+    if not os.path.isdir(args.Dir):
+        print("Directory %s does not exist" % args.Dir)
+        sys.exit(1)
+    
+    # server begins listening for incoming TCP requests
+    serverSocket.listen(1)
+
+    print("Server started")
+    print("Waiting for client request...")
+    while True:
+        # server waits for incoming requests; new socket created on return
+        connectionSocket, addr = serverSocket.accept()
+        new_thread = ClientThread(addr, connectionSocket)
+        new_thread.start()
+
+    try: 
+        serverSocket.close()
+    except error as e: 
+        print ("Error closing socket: %s" % e) 
+        sys.exit(1)
+
+if __name__ == '__main__':
+    server()
