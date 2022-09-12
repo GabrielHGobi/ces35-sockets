0b117e80bf18061d4b3419e02731263aa6c5eb95
GabrielHGobi <gabrielhgobi@gmail.com>
2022-09-12 15:45:52 -0300

    URL parsing on web-client

--- web-client.py
+++ web-client.py
@@ -1,14 +1,44 @@
-import argparse
+from argparse import ArgumentParser
+from socket import *
+import re
+import sys
 
-server_parser = argparse.ArgumentParser(description='List URL\'s to files to be requested by the client')
+def urlparse(URL):
+    url_parsed = {}
+    m_url = re.search("(http|https)://(.*)/(.*)", URL)
+    if m_url is None:
+        print("Invalid URL: %s" % URL)
+        sys.exit(1)
+    else:
+        netloc = m_url.group(2)
+        m_netloc = re.search("([a-zA-Z0-9]+)(?::([1-9][0-9]*))?", netloc)
+        if m_netloc is None:
+            print("Invalid address: %s" % netloc)
+            sys.exit(1)
+        else:
+            url_parsed['scheme'] = m_url.group(1)
+            url_parsed['hostname'] = m_netloc.group(1)
+            if m_netloc.group(2) != '':
+                url_parsed['port'] = int(m_netloc.group(2))
+            else:
+                url_parsed['port'] = 8080 #default HTTP port
+            if m_url.group(3) != '':
+                url_parsed['path'] = m_url.group(3)
+            else:
+                url_parsed['path'] = 'index.html'
+            
+            return url_parsed
+
+
 
 def client():
     server_parser = ArgumentParser(description='List URL\'s to files to be requested by the client')
     server_parser.add_argument('URL_list', metavar='URL', type=str,  nargs='+',
-                            help='file URL to be retrieved')
+                                help='file URL to be retrieved')
     args = server_parser.parse_args()
 
     for URL in args.URL_list:
+        url_parsed = urlparse(URL)
 
         # server machine's name 
         serverHostname = url_parsed['hostname']
